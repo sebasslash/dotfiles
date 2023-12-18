2fe57270fe25952cbd8ba73c1e92deb0cd7ecff0
Sebastian Rivera <sebas.rivera0047@gmail.com>
2023-12-17 20:01:08 -0500

    Added init script to link config dirs

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## Careful
+
+ln -sf ~/dotfiles/nvim ~/.config/nvim
+ln -sf ~/dotfiles/awesome ~/.config/awesome
+ln -sf ~/dotfiles/alacritty ~/.config/alacritty
+ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
